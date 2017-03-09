c886470c5b1d64a871de9861d7b4236f2ef10ae9
Chris Scanlin <cscanlin@gmail.com>
2017-03-09 13:32:44 -0800

    fix allowed hosts

--- films_api/settings.py
+++ films_api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.getenv('FILMS_API_SECRET')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'films-api.herokuapp.com']
 
 
 # Application definition
