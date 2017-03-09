5ac823612eb6becec9823d4ea17b5c14d81497da
Chris Scanlin <cscanlin@gmail.com>
2017-03-09 14:03:44 -0800

    fix db settings for dev

--- films_api/settings.py
+++ films_api/settings.py
@@ -80,14 +80,15 @@ WSGI_APPLICATION = 'films_api.wsgi.application'
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 
-DATABASES = {
-    'default': {
+environment_type = os.getenv('DJANGO_CONFIGURATION', 'dev')
+DATABASES = {}
+if environment_type == 'dev':
+    DATABASES['default'] = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-}
-
-DATABASES['default'].update(db_from_env)
+elif environment_type in ['Prod', 'Stage']:
+    DATABASES['default'].update(db_from_env)
 
 
 # Password validation
