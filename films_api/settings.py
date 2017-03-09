676aa84ffe16af1a62195c247f8cd79aec098c2f
Chris Scanlin <cscanlin@gmail.com>
2017-03-09 13:49:42 -0800

    more static fixes

--- films_api/settings.py
+++ films_api/settings.py
@@ -126,7 +126,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (STATIC_ROOT,)
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
