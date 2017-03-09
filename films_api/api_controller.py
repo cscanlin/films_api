2c71f30be9b4cc53ab50eeef7a554cb3fc61cc03
Chris Scanlin <cscanlin@gmail.com>
2017-03-09 13:00:20 -0800

    add RatingFilter

--- films_api/api_controller.py
+++ films_api/api_controller.py
@@ -14,7 +14,6 @@ class IntegerListFilter(django_filters.Filter):
             return qs.filter(**{'{}__{}'.format(self.name, self.lookup_expr): integers})
         return qs
 
-
 class FilmFilter(FilterSet):
     ids = IntegerListFilter(name="id", lookup_expr='in')
     min_year = django_filters.NumberFilter(name="year", lookup_expr='gte')
@@ -25,6 +24,15 @@ class FilmFilter(FilterSet):
     class Meta:
         model = Film
         fields = ['ids', 'min_year', 'max_year', 'title', 'description']
+
+class RatingFilter(FilterSet):
+    ids = IntegerListFilter(name="id", lookup_expr='in')
+    min_score = django_filters.NumberFilter(name="score", lookup_expr='gte')
+    max_score = django_filters.NumberFilter(name="score", lookup_expr='lte')
+
+    class Meta:
+        model = Rating
+        fields = ['ids', 'min_score', 'max_score']
 
 class FilmList(generics.ListCreateAPIView):
     queryset = Film.objects.all().prefetch_related('related_films').annotate(average_score=Avg('ratings__score'))
@@ -41,13 +49,19 @@ class FilmDetail(generics.RetrieveUpdateDestroyAPIView):
 class RatingList(generics.ListCreateAPIView):
     queryset = Rating.objects.all()
     serializer_class = RatingSerializer
+    filter_backends = (DjangoFilterBackend,)
+    filter_class = RatingFilter
 
 class RatingDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Rating.objects.all()
     serializer_class = RatingSerializer
+    filter_backends = (DjangoFilterBackend,)
+    filter_class = RatingFilter
 
 class FilmRatingList(mixins.ListModelMixin, generics.GenericAPIView):
     serializer_class = FilmRatingSerializer
+    filter_backends = (DjangoFilterBackend,)
+    filter_class = RatingFilter
 
     def get_queryset(self):
         return Film.objects.get(**self.kwargs).ratings.all()
