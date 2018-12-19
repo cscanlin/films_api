d39c43a559d44d3c71e39797463c100f584dbd24
Chris Scanlin <cscanlin@gmail.com>
2018-12-18 21:56:14 -0800

    re-implement basic filters

--- films/api_controller.py
+++ films/api_controller.py
@@ -2,43 +2,33 @@ from .models import Film, Rating
 from .serializers import RootFilmSerializer, RatingSerializer, FilmRatingSerializer
 from rest_framework import generics, mixins, filters, status
 from rest_framework.response import Response
-import django_filters
-from django_filters.rest_framework import FilterSet, DjangoFilterBackend
+from django_filters.rest_framework import DjangoFilterBackend, FilterSet
 from django.db.models import Avg
 from django.http import HttpResponseRedirect
 
 def home(request):
+    return None
     return HttpResponseRedirect('/docs')
-
-class IntegerListFilter(django_filters.Filter):
-    # http://stackoverflow.com/a/24042182/1883900
-    def filter(self, qs, value):
-        if value not in (None, ''):
-            integers = [int(v) for v in value.split(',')[:100]]
-            return qs.filter(**{'{}__{}'.format(self.name, self.lookup_expr): integers})
-        return qs
 
 # The filters are practically self-documenting!
 
 class FilmFilter(FilterSet):
-    ids = IntegerListFilter(name="id", lookup_expr='in')
-    min_year = django_filters.NumberFilter(name="year", lookup_expr='gte')
-    max_year = django_filters.NumberFilter(name="year", lookup_expr='lte')
-    title = django_filters.CharFilter(name="title", lookup_expr='icontains')
-    description = django_filters.CharFilter(name="description", lookup_expr='icontains')
 
     class Meta:
         model = Film
-        fields = ['ids', 'min_year', 'max_year', 'title', 'description']
+        fields = {
+            'year': ['lte', 'gte'],
+            'title': ['icontains'],
+            'description': ['icontains'],
+        }
 
 class RatingFilter(FilterSet):
-    ids = IntegerListFilter(name="id", lookup_expr='in')
-    min_score = django_filters.NumberFilter(name="score", lookup_expr='gte')
-    max_score = django_filters.NumberFilter(name="score", lookup_expr='lte')
 
     class Meta:
         model = Rating
-        fields = ['ids', 'min_score', 'max_score']
+        fields = {
+            'score': ['lte', 'gte'],
+        }
 
 # These classes contain the bulk of the logic and are the most common place for customization
 # Each class inherits from either a `ListCreateAPIView` class for general endpoints,
@@ -52,31 +42,27 @@ class FilmList(generics.ListCreateAPIView):
     # and the average rating all in one query.
     queryset = Film.objects.all().prefetch_related('related_films').annotate(average_score=Avg('ratings__score'))
     serializer_class = RootFilmSerializer
-    # filter_backends = (DjangoFilterBackend, filters.OrderingFilter)
-    # filter_class = FilmFilter
+    filter_backends = (DjangoFilterBackend, filters.OrderingFilter)
+    filter_class = FilmFilter
 
 class FilmDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Film.objects.all().prefetch_related('related_films').annotate(average_score=Avg('ratings__score'))
     serializer_class = RootFilmSerializer
-    # filter_backends = (DjangoFilterBackend,)
-    # filter_class = FilmFilter
+    filter_class = FilmFilter
 
 class RatingList(generics.ListCreateAPIView):
     queryset = Rating.objects.all()
     serializer_class = RatingSerializer
-    # filter_backends = (DjangoFilterBackend,)
-    # filter_class = RatingFilter
+    filter_class = RatingFilter
 
 class RatingDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Rating.objects.all()
     serializer_class = RatingSerializer
-    # filter_backends = (DjangoFilterBackend,)
-    # filter_class = RatingFilter
+    filter_class = RatingFilter
 
 class FilmRatingList(mixins.ListModelMixin, generics.GenericAPIView):
     serializer_class = FilmRatingSerializer
-    # filter_backends = (DjangoFilterBackend,)
-    # filter_class = RatingFilter
+    filter_class = RatingFilter
 
     def get_queryset(self):
         # Only return ratings for the specific film
