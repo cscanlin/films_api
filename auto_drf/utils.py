fb04f6b9af7e49acea31d1f71ddefbae477fb4d3
Chris Scanlin <cscanlin@gmail.com>
2019-03-13 02:24:22 -0700

    WIP calculated_properties filter

--- auto_drf/utils.py
+++ auto_drf/utils.py
@@ -4,6 +4,12 @@ from django.db.models.fields.reverse_related import ManyToOneRel, ManyToManyRel
 RELATED_FIELD_TYPES = (RelatedField, ManyToOneRel, ManyToManyRel)
 
 def dynamic_field_filters(model):
-    return {field.name: field.__class__.class_lookups.keys()
-            for field in model._meta.get_fields()
-            if not isinstance(field, RELATED_FIELD_TYPES)}
+    field_filters = {field.name: field.__class__.class_lookups.keys()
+                     for field in model._meta.get_fields()
+                     if not isinstance(field, RELATED_FIELD_TYPES)}
+    # if hasattr(model, 'calculated_properties'):
+    #     property_filters = {field_name: field_type.class_lookups.keys()
+    #                         for field_name, (field_type, _)
+    #                         in model.calculated_properties().items()}
+    #     field_filters.update(property_filters)
+    return field_filters
