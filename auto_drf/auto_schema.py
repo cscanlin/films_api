22acf0f7e9aa35d3fb0593ba01af46a22b039d86
Chris Scanlin <cscanlin@gmail.com>
2019-03-17 13:26:25 -0700

    WIP - not working AutoDRFSchemaGenerator

--- auto_drf/auto_schema.py
+++ auto_drf/auto_schema.py
@@ -1,18 +1,22 @@
 from django.conf import settings
 
 from rest_framework.decorators import api_view, renderer_classes
-from rest_framework.renderers import OpenAPIRenderer, JSONOpenAPIRenderer
+from rest_framework.renderers import JSONOpenAPIRenderer
 from rest_framework.schemas import SchemaGenerator
 from rest_framework.response import Response
 
-
 class AutoDRFSchemaGenerator(SchemaGenerator):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
 
     def get_links(self, request):
         links = super().get_links(request)
-        print(vars(links['films']))
+        for document_name, node in links.items():
+            for method, link in node.links:
+                if str(method) != 'list':
+                    continue
+                for field in link.fields:
+                    if '__' in field.name:
+                        field.description = '!!Filter' + (field.description or '')
+                    print(field)
         return links
 
 
